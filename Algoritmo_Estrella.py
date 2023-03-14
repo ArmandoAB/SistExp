a86e7cf343f45632918b7d9a1d7309b09a0165e4
ArmandoAB <112527374+ArmandoAB@users.noreply.github.com>
2023-03-13 18:53:43 -0600

    Ejemplo de algoritmo estrella
    El programa busca el camino más corto a través de una ciudad a pesar de posibles calles cerradas o tráfico

---
+++ Algoritmo_Estrella.py
@@ -0,0 +1,45 @@
+import heapq
+
+class Graph:
+    def __init__(self):
+        self.edges = {}
+        self.weights = {}
+        
+    def add_edge(self, from_node, to_node, weight):
+        if from_node not in self.edges:
+            self.edges[from_node] = []
+        self.edges[from_node].append(to_node)
+        self.weights[(from_node, to_node)] = weight
+        
+def A_star(graph, start, end):
+    queue = [(0, start, [])]
+    visited = set()
+    
+    while queue:
+        (cost, current_node, path) = heapq.heappop(queue)
+        if current_node == end:
+            return path + [current_node]
+        if current_node not in visited:
+            visited.add(current_node)
+            for neighbor in graph.edges[current_node]:
+                if neighbor not in visited:
+                    total_cost = graph.weights[(current_node, neighbor)] + cost
+                    heuristic_cost = heuristic(neighbor, end)
+                    priority = total_cost + heuristic_cost
+                    heapq.heappush(queue, (priority, neighbor, path + [current_node]))
+                    
+def heuristic(node, goal):
+    # Distancia en línea recta entre dos puntos
+    x1, y1 = node
+    x2, y2 = goal
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+
+g = Graph()
+g.add_edge((1, 1), (1, 2), 1)
+g.add_edge((1, 2), (2, 2), 2)
+g.add_edge((2, 2), (3, 3), 3)
+g.add_edge((1, 1), (2, 2), 2)
+g.add_edge((2, 2), (1, 1), 2)
+path = A_star(g, (1, 1), (3, 3))
+print(path)
+
